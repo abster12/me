e7ddec6ed3af277b990b156e56163bbbfc6686f5
Abhigyan <abhigyan0987@gmail.com>
2019-09-13 18:19:26 +0530

    add files

---
+++ src/components/timeline.jsx
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+
+export default class Timeline extends Component {
+  render() {
+    return (
+      <div>
+        <section className="colorlib-experience" data-section="timeline">
+          <div className="colorlib-narrow-content">
+            <div className="row">
+              <div className="col-md-6 col-md-offset-3 col-md-pull-3 animate-box" data-animate-effect="fadeInLeft">
+                <span className="heading-meta">highlights</span>
+                <h2 className="colorlib-heading animate-box">Timeline</h2>
+              </div>
+            </div>
+            <div className="row">
+              <div className="col-md-12">
+                <div className="timeline-centered">
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-3">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Internship at Optum <span>May 2019 - July 2019</span></h2>
+                        <ul>
+                        <li>Designed an automated DevOps pipeline,reducing the amount of time for a deployment by 50% and increasing deployments per day by 100%.</li>
+                        <li>Set up a scalable and highly available cloud infrastructure,reducing costs by upto 50% while improving overall performance of the product.</li>
+                        <li>Tools: ELK Stack,Prometheus,Grafana,Kubernetes,Docker,Git,GitHub.  </li>         
+                        </ul>           
+                    </div>
+                    </div>
+                  </article>
+                  <article className="timeline-entry animate-box" data-animate-effect="fadeInTop">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-4">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Undergraduation at IIT(BHU)<span>July 2016- June 2020</span></h2>
+                        <p>I am pursuing my under-graduation studies with major in engineering and I have been working various different projects and learning different skills.</p>
+                      </div>
+                    </div>
+                  </article>
+                  {/* <article className="timeline-entry animate-box" data-animate-effect="fadeInLeft">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-5">
+                        <i className="icon-pen2" />
+                      </div>
+                      <div className="timeline-label">
+                        <h2>Primary and Higher Education <span>2003-2015</span></h2>
+                        <p>I have completed my higher secondary education with major subjects as Physics,Chemistry & Maths with 91 merit. During my time at school, I have developed interest in solving complex problems of the fundamental physics which helped me to improve my understanding of any problem and also my mathematical skills to actually solve the problems.</p> 
+                      </div>
+                    </div>
+                  </article> */}
+                  <article className="timeline-entry begin animate-box" data-animate-effect="fadeInBottom">
+                    <div className="timeline-entry-inner">
+                      <div className="timeline-icon color-none">
+                      </div>
+                    </div>
+                  </article>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    )
+  }
+}
