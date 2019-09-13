dad53149655b13acdc5231eb4bb09f210deef9f5
abhigyan <abhigyan0987@gmail.com>
2019-09-13 18:40:08 +0530

    Updates

---
+++ precache-manifest.b41b0c0753af2181d57ac381abcc956b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1caf0789ee8fdb1f0b3c",
+    "url": "/me/static/js/runtime~main.1caf0789.js"
+  },
+  {
+    "revision": "66e6af6315ad964a63ff",
+    "url": "/me/static/js/main.66e6af63.chunk.js"
+  },
+  {
+    "revision": "5ec21d738fba382f2658",
+    "url": "/me/static/js/2.5ec21d73.chunk.js"
+  },
+  {
+    "revision": "66e6af6315ad964a63ff",
+    "url": "/me/static/css/main.b1ad718d.chunk.css"
+  },
+  {
+    "revision": "12fd5b990b5392ee63d7a62b01d63f24",
+    "url": "/me/index.html"
+  }
+];
